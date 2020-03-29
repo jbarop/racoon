e37ee01f76c89cbf620008bd307dda6bc1c9f219
Johannes Barop <johannes.barop@codecentric.de>
2020-03-30 00:37:43 +0200

    load data with d3, display deaths in Germany in the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React, {useEffect, useState} from 'react';
-import Papa from 'papaparse';
+import * as d3 from 'd3';
 
 const dataSource = `${process.env.PUBLIC_URL}/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv`;
 
+interface Data {
+  date: Date;
+  value: number;
+}
+
 function App() {
-  const [data, setData] = useState<string[][]>();
+  const [data, setData] = useState<Data[]>();
   useEffect(() => {
-    fetch(dataSource)
-      .then(response => response.text())
-      .then(text => setData(Papa.parse(text).data))
-      .catch(reason => console.error(reason));
+    d3.csv(dataSource)
+      .then(csvRows => {
+        let timeFmt = d3.timeParse("%m/%d/%Y");
+        let row = csvRows.find(row => row['Country/Region'] === 'Germany')!;
+        setData(
+          Object.entries(row).map((key, value) =>
+            ({
+              date: timeFmt(key[0]),
+              value: parseInt(key[1]!)
+            } as Data))
+            .filter(value => value.date != null)
+        );
+      })
+
   }, []);
 
   return !data
@@ -21,17 +36,15 @@ function App() {
           <table>
             <thead>
             <tr>
-              {data[0].map(headline => (
-                <th key={headline}>{headline}</th>
-              ))}
+              <th>Date</th>
+              <th>Deaths</th>
             </tr>
             </thead>
             <tbody>
-            {data?.slice(1).map((row, rowIndex) => (
-              <tr key={`row-${rowIndex}`}>
-                {row.map((col, colIndex) => (
-                  <td key={`row-${rowIndex}-col-${colIndex}`}>{col}</td>
-                ))}
+            {data!.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                <td>{row.date.toISOString()}</td>
+                <td>{row.value}</td>
               </tr>
             ))}
             </tbody>
