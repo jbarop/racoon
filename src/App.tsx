3835ba219eb29e7f5c3b75003173bb4fae9bb217
Johannes Barop <johannes.barop@codecentric.de>
2020-03-29 23:59:51 +0200

    add mock data based on Johns Hopkins University Center for Systems Science and Engineering

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Papa from 'papaparse';
+
+const dataSource = `${process.env.PUBLIC_URL}/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv`;
 
 function App() {
-  return (
-    <>
-      <header className="App-header">
-        <h1>racoon - Corona Dashboard</h1>
-      </header>
-    </>
-  );
+  const [data, setData] = useState<string[][]>();
+  useEffect(() => {
+    fetch(dataSource)
+      .then(response => response.text())
+      .then(text => setData(Papa.parse(text).data))
+      .catch(reason => console.error(reason));
+  }, []);
+
+  return !data
+    ? <span>Loading</span>
+    : (
+      <>
+        <header className="App-header">
+          <h1>racoon - Corona Dashboard</h1>
+          <table>
+            <thead>
+            <tr>
+              {data[0].map(headline => (
+                <th key={headline}>{headline}</th>
+              ))}
+            </tr>
+            </thead>
+            <tbody>
+            {data?.slice(1).map((row, rowIndex) => (
+              <tr key={`row-${rowIndex}`}>
+                {row.map((col, colIndex) => (
+                  <td key={`row-${rowIndex}-col-${colIndex}`}>{col}</td>
+                ))}
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        </header>
+      </>
+    );
 }
 
 export default App;
