c19e8bcd15f4096be1659d16e231fbf144781726
Johannes Barop <johannes.barop@codecentric.de>
2020-03-30 18:43:09 +0200

    parse and group records of all countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,67 @@
 import React, {useEffect, useRef, useState} from 'react';
 import * as d3 from 'd3';
+import {DSVRowArray, DSVRowString} from 'd3';
 
 const dataSource = `${process.env.PUBLIC_URL}/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv`;
 
-interface Data {
+interface Record {
   date: Date;
   value: number;
 }
 
+interface Countries {
+  [key: string]: Record[]
+}
+
+/**
+ * Converts a row from the CSV into an array of {@link Record}s.
+ *
+ * A row has a few columns of meta information and one column per day.
+ */
+function csvRowToRecords(row: DSVRowString): { countryName: string, records: Record[] } {
+  const parseTime = d3.timeParse("%m/%d/%Y");
+
+  return {
+    countryName: row['Country/Region']!,
+    records: Object.entries(row)
+      .map(key =>
+        ({
+          date: parseTime(key[0]),
+          value: parseInt(key[1] as string)
+        } as Record))
+      .filter(it => it.date != null)
+  };
+}
+
+/**
+ * Converts a CSV into an object which holds time series {@link Record}s for each country.
+ *
+ * Some countries are split across multiple rows. These are summed together.
+ */
+function csvToCountries(rows: DSVRowArray): Countries {
+  return rows
+    .map(row => csvRowToRecords(row))
+    .reduce((accumulator, it) => {
+      // group by by country name because some countries are split across multiple rows
+      accumulator[it.countryName] = !accumulator[it.countryName]
+        ? it.records // new country --> insert it
+        : accumulator[it.countryName] // existing country --> add the values to the existing records.
+          .map((day, index) => ({...day, value: day.value + it.records[index].value}));
+      return accumulator;
+    }, {} as Countries)
+}
+
 function App() {
-  const [data, setData] = useState<Data[]>();
+  const [countries, setCountries] = useState<Countries>();
   const graphContainer = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     d3.csv(dataSource)
-      .then(csvRows => {
-        let timeFmt = d3.timeParse("%m/%d/%Y");
-        let row = csvRows.find(row => row['Country/Region'] === 'Germany')!;
-        setData(
-          Object.entries(row).map((key, value) =>
-            ({
-              date: timeFmt(key[0]),
-              value: parseInt(key[1]!)
-            } as Data))
-            .filter(value => value.date != null)
-        );
-      })
+      .then((csvRows: DSVRowArray) => setCountries(csvToCountries(csvRows)))
   }, []);
 
   useEffect(() => {
-    if (!graphContainer.current) {
+    if (!graphContainer.current || !countries) {
       return;
     }
 
@@ -42,36 +74,46 @@ function App() {
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    const dataGermany = countries['Germany'];
+    const dataItaly = countries['Italy'];
 
     const x = d3.scaleTime()
-      .domain(d3.extent(data!, datum => datum.date) as [Date, Date])
+      .domain(d3.extent(dataItaly, datum => datum.date) as [Date, Date])
       .range([0, width]);
 
     const y = d3.scaleLinear()
-      .domain([0, d3.max(data!, d => d.value) as number])
+      .domain([1, d3.max(dataItaly, d => d.value)!])
       .range([height, 0]);
 
-    const line = d3.line<Data>()
+    const line = d3.line<Record>()
       .x(d => x(d.date))
       .y(d => y(d.value));
 
     svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", `translate(0, ${height})`)
       .call(d3.axisBottom(x));
 
     svg.append("g")
       .call(d3.axisLeft(y));
 
     svg.append("path")
-      .datum(data!)
+      .datum(dataGermany)
       .attr("fill", "none")
       .attr("stroke", "steelblue")
       .attr("stroke-width", 1.5)
       .attr("d", line);
-  }, [data, graphContainer]);
 
-  return !data
+    svg.append("path")
+      .datum(dataItaly)
+      .attr("fill", "none")
+      .attr("stroke", "red")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+  }, [countries, graphContainer]);
+
+  return !countries
     ? <span>Loading</span>
     : (
       <>
