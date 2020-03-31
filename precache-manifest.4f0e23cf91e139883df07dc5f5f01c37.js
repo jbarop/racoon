89c8038abaf3ff390416fff725ad0604628924f6
Johannes Barop <jb@barop.de>
2020-03-31 07:54:18 +0000

    Uploading new version: d3394b2fcaec8e8a627139dfe5198fa47f601d0c

---
+++ precache-manifest.4f0e23cf91e139883df07dc5f5f01c37.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9216b1676dfb0057bccb7fc469c57d",
+    "url": "/racoon/index.html"
+  },
+  {
+    "revision": "eab040cd0d9fdfa3445c",
+    "url": "/racoon/static/css/2.40636a32.chunk.css"
+  },
+  {
+    "revision": "eab040cd0d9fdfa3445c",
+    "url": "/racoon/static/js/2.a90463a2.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/racoon/static/js/2.a90463a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd21702e3d147d8cf06",
+    "url": "/racoon/static/js/main.b06f2ef8.chunk.js"
+  },
+  {
+    "revision": "7d3c92bd36b4e1efab92",
+    "url": "/racoon/static/js/runtime-main.86e2565e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/racoon/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/racoon/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/racoon/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/racoon/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/racoon/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/racoon/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/racoon/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/racoon/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/racoon/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/racoon/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/racoon/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/racoon/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/racoon/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/racoon/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/racoon/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/racoon/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
